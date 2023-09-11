4b999376ce68b70ee817487ebe43de81a72145f6
ElifKumbetli <elifkumbetli@gmail.com>
2023-09-11 18:43:32 +0300

    11 Eylül

---
+++ OOP1/Product.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP1
+{
+    //snipped
+    internal class Product // Entity
+    {
+        public int Id { get; set; }
+
+        public int CategoryId { get; set; }
+
+        //UnitPrice --> Ürünün birim fiyatı 
+        public double UnitPrice { get; set; }
+
+        //UnitsInStock --> Ürünün stok adeti
+        public double UnitsInStock { get; set; }
+        public string ProductName { get; internal set; }
+
+        //CRUD --> Create, Read, Update, Delete
+    }
+}
