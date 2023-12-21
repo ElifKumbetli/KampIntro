844341dea9df448583ad2ccdf45a92b58e390ff4
ElifKumbetli <elifkumbetli@gmail.com>
2023-12-21 15:03:20 +0300

    değişiklikler

---
+++ GameProject/Gamer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameProject
+{
+    internal class Gamer
+    {
+        internal string FirstName;
+
+        public int Id { get; set; }
+
+        public string Firstname { get; set; }
+
+        public string LastName { get; set; }
+
+        public int BirthYear { get; set; }
+
+        public int IdentityNumber { get; set; }
+
+    }
+}
---
+++ GameProject/IGamerService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameProject
+{
+    internal interface IGamerService
+    {
+        //Operasyonların interfacesini yazıyoruz.
+        void Add(Gamer gamer);//Kayıt
+
+        void Update(Update gamer);//Güncelleme
+
+        void Delete(Gamer gamer);//Silmek
+
+    }
+}
